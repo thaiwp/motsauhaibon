8da5fbcc9f420c3c4096bd1614b63e9b5e0264d4
thaiwp <92037064+thaiwp@users.noreply.github.com>
2021-12-02 17:20:12 +0700

    Add files via upload

---
+++ my_license_info.php
@@ -0,0 +1,4 @@
+
+<!-- saved from url=(0049)https://sharecode.win/install/my_license_info.php -->
+<html><head></head>
+<body>error=0 lid=111006 uid=35369 os=ES+7.0+64 ip=103.161.172.179 hostname=sv3.saigondata.net</body></html>
